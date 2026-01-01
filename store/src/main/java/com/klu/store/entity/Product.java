1ec7b23b5d7dfbff4e018df02c8fca1f5055e36a
SirishaDuba <sirishaduba29@gmail.com>
2026-01-01 10:52:38 +0530

    Initial message

---
+++ store/src/main/java/com/klu/store/entity/Product.java
@@ -0,0 +1,71 @@
+package com.klu.store.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "skill_product")
+public class Product {
+	
+	@Id
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int pid;
+	
+	@Column(length = 20,nullable = false)
+	private String name;
+	
+	
+	@Column(length=100)
+	private String description;
+	
+	private double price;
+	
+	private int quantity;
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
